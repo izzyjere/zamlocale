a718495c1c47cbc432e2d3bbc1fac4e96c3963ba
izzyjere <wisdomjere5@gmail.com>
2023-03-08 10:36:43 +0200

    Create project

---
+++ src/main/java/com/guavasoft/zamlocale/ZamlocaleApplication.java
@@ -0,0 +1,13 @@
+package com.guavasoft.zamlocale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZamlocaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZamlocaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guavasoft/zamlocale/ZamlocaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guavasoft.zamlocale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZamlocaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
