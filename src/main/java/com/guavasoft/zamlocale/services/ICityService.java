6942e339b615510d5df5d9be550ddcad1344b3a2
izzyjere <wisdomjere5@gmail.com>
2023-03-08 11:17:56 +0200

    refactor

---
+++ src/main/java/com/guavasoft/zamlocale/services/ICityService.java
@@ -0,0 +1,5 @@
+package com.guavasoft.zamlocale.services;
+import com.guavasoft.zamlocale.models.City;
+
+public interface ICityService extends  IService<City>{
+}
--- src/main/java/com/guavasoft/zamlocale/services/imp/CityService.java
+++ src/main/java/com/guavasoft/zamlocale/services/imp/CityService.java
@@ -1,13 +1,14 @@
 package com.guavasoft.zamlocale.services.imp;
 
 import com.guavasoft.zamlocale.models.City;
+import com.guavasoft.zamlocale.services.ICityService;
 import com.guavasoft.zamlocale.services.IService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class CityService implements IService<City> {
+public class CityService implements ICityService {
     @Override
     public City getById(long id) {
         return null;
