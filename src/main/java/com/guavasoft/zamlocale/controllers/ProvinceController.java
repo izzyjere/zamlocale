ecb0cbbf438369f03db6a067fe29798abe025f9b
izzyjere <wisdomjere5@gmail.com>
2023-03-09 15:47:04 +0200

    province controller finish ups

--- src/main/java/com/guavasoft/zamlocale/controllers/ProvinceController.java
+++ src/main/java/com/guavasoft/zamlocale/controllers/ProvinceController.java
@@ -3,12 +3,12 @@ package com.guavasoft.zamlocale.controllers;
 import com.guavasoft.zamlocale.contracts.Result;
 import com.guavasoft.zamlocale.models.Province;
 import com.guavasoft.zamlocale.services.IProvinceService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/province")
@@ -24,5 +24,25 @@ public class ProvinceController {
           return  new ResponseEntity<>(created, HttpStatusCode.valueOf(200));
       }
       return  new ResponseEntity<>(null,HttpStatusCode.valueOf(500));
+    }
+    @GetMapping
+    public  ResponseEntity<List<Province>> GetAll (){
+       return new ResponseEntity<>(service.getAll(),HttpStatusCode.valueOf(200));
+    }
+    @GetMapping("getById")
+    public  ResponseEntity<Province> GetById(@PathParam("id") long id){
+        var record = service.getById(id);
+        if(record!=null) {
+            return  new ResponseEntity<>(record,HttpStatusCode.valueOf(200));
+        }
+        return  new ResponseEntity<>(HttpStatusCode.valueOf(401));
+    }
+    @PutMapping
+    public  ResponseEntity<Province> Update(@PathParam("id") long id, Province entity){
+        var updated = service.update(entity,id);
+        if(updated!=null){
+            return  new ResponseEntity<>(updated, HttpStatusCode.valueOf(200));
+        }
+        return new ResponseEntity<>(HttpStatusCode.valueOf(400));
     }
 }
